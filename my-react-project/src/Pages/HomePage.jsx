f330fc013b99db99d924d2a76f09107eb5e57d25
sudharsan1306 <sudharsan1113@gmail.com>
2026-08-16 22:14:10 +0530

    add

---
+++ my-react-project/src/Pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return(
+        <>
+          <h3>hello</h3>
+          <p> wellcome to react js</p>
+        </>
+    )
+}
---
+++ my-react-project/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import BlogsPage from './Pages/BlogaPage';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+      //<HomePage/>
+      // <AboutPage/>
+       <BlogsPage/>
+);
+
