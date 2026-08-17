1d0c2ffe069ed96f8fbfff639abbaa9f8ca05471
sudharsan1306 <sudharsan1113@gmail.com>
2026-08-17 20:44:32 +0530

    B complete

---
+++ my-react-project/src/Pages/B_react use.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function B_reactUse(){
+
+    const[product,setProduct] = useState ("chocalate")
+    const[qty,setQty]         = useState (1)
+    const [price,setprice]    = useState (10)
+
+
+    return(
+
+        <>
+        <div>
+            <h1> Order Details </h1>
+         
+        </div>
+
+        <div>
+            <button onClick={() => setQty(qty > 0 ? qty- 1 : 0)}>-</button>
+            <span>{qty}</span>
+            <button onClick={ () => setQty(qty + 1)}> + </button>
+        </div>
+
+        <div>
+            <h4>
+                total price : {qty * price}
+            </h4>
+        </div>
+        </>
+    )
+}  
--- my-react-project/src/index.js
+++ my-react-project/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import A_jsUse from './Pages/A_js use';
+import B_react from './Pages/B_react use';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <A_jsUse/>
+      <B_react/>
 );
 
